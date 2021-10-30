6ef257188a6e981c2ee1ab8000644894755b2c41
Elon Aseneka <elonsantos63@gmail.com>
2021-10-30 23:38:32 +0300

    feat(core): reusable controlled forms

---
+++ src/components/Form.tsx
@@ -0,0 +1,83 @@
+import { Visibility, VisibilityOff } from "@mui/icons-material";
+import { Button, IconButton, InputAdornment, TextField } from "@mui/material";
+import React, { FunctionComponent, useContext, useState } from "react";
+
+interface FormProps {
+    children: any;
+    submit: any;
+    initialValues: any;
+    buttonText: string;
+}
+
+interface InputFieldProps {
+    type: string;
+    variant: 'standard' | 'outlined' | 'filled';
+    label?: string;
+    name: string;
+    size: 'small' | 'medium';
+}
+
+export const FormContext = React.createContext({
+    form: {} as any,
+    handleFormChange: {} as any,
+});
+
+export const InputField: FunctionComponent<InputFieldProps> = (props: InputFieldProps) => {
+    const formContext = useContext(FormContext);
+    const { form, handleFormChange } = formContext;
+    const { type, variant, label, name, size } = props;
+
+    const handleMouseDownPassword = (event: React.MouseEvent<HTMLButtonElement>) => {
+        event.preventDefault();
+    };
+
+    return (
+        type !== 'password' ? <TextField color="secondary" size={size} value={form[name]} type={type} label={label} variant={variant} onChange={handleFormChange} /> : <TextField color="secondary" size={size} value={form[name]} type={form['showPassword'] ? 'text' : type} label={label} variant={variant} onChange={handleFormChange} InputProps={{
+            endAdornment: <InputAdornment position="end"><IconButton
+                aria-label="toggle password visibility"
+                onClick={handleFormChange}
+                onMouseDown={handleMouseDownPassword}
+                edge="end"
+            >
+                {form['showPassword'] ? <VisibilityOff /> : <Visibility />}
+            </IconButton></InputAdornment>,
+        }} />
+    );
+};
+
+const Form: FunctionComponent<FormProps> = (props: FormProps) => {
+    const { children, submit = () => { }, initialValues, buttonText } = props;
+
+    const [form, setForm] = useState(initialValues);
+
+    const handleFormChange = <T,>(event: T) => {
+        const type = 'type' as keyof T;
+        const target = 'target' as keyof T;
+
+        if (event[type] as any === 'change') {
+            const { name, value } = event[target] as any;
+
+            setForm({
+                ...form,
+                [name]: value
+            });
+        } else {
+            setForm({showPassword: !form.showPassword});
+        }
+    };
+
+    return (
+        <form>
+            <FormContext.Provider value={{
+                form,
+                handleFormChange
+            }}>
+                {children}
+            </FormContext.Provider>
+
+            <Button variant="contained" color="secondary" onClick={() => submit(form)}>{buttonText}</Button>
+        </form>
+    );
+}
+
+export default Form;
