9f2bb181ebef2a30fbfb1f2b53192bb95c80ada6
Elon Aseneka <elonsantos63@gmail.com>
2021-12-19 22:07:58 +0300

    feat(Sponsors): bullying experience tab

--- src/views/BullyingExperience.tsx
+++ src/views/BullyingExperience.tsx
@@ -2,9 +2,9 @@ import React, { FunctionComponent, useEffect } from "react";
 import Axios from 'axios';
 
 interface BullyingExperienceProps {
-    
+
 }
- 
+
 const BullyingExperience: FunctionComponent<BullyingExperienceProps> = () => {
     const [response, setResponse] = React.useState({} as any);
     const [loading, setLoading] = React.useState(false);
@@ -45,10 +45,20 @@ const BullyingExperience: FunctionComponent<BullyingExperienceProps> = () => {
     }, [selectedPage]);
 
     return (
-        <div  style={{ width: '100%'}}>
+        <div style={{ width: '100%' }}>
+            <div style={{width: '60%', marginLeft: 'auto', marginRight: 'auto'}}>
+                If you are the parent/guardian of a student who has been bullied we
+                are interested in your story. We are especially interested in situations
+                where the parents/guardians have made every effort to work with
+                school administrators to get the abuse stopped but the bullying
+                continued. Please use the form below to share your story.
+                _______________ email address
+                Tell us about your experience and please include any advice you may
+                have for us to make our system work even better for you:
+            </div>
             <div dangerouslySetInnerHTML={{ __html: response?.content }}></div>
         </div>
     );
 }
- 
+
 export default BullyingExperience;
